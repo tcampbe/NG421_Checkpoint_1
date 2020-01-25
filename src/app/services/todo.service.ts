fee666f1203bca55ea6b2f006cc0b1a84059955a
tcampbe <tcampb@icloud.com>
2020-01-25 13:26:10 -0600

    code section done

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,30 +1,38 @@
-import { Injectable } from '@angular/core';
-import {ITodo} from '../interfaces/itodo';
+import { Injectable } from "@angular/core";
+import { ITodo } from "../interfaces/itodo";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class TodoService {
   todoId: number = 0;
-  todoList: ITodo [] = [
+  todoList: ITodo[] = [
     // example of how to make an item in todo list
-    { title: 'Install Angular CLI', 
-    id: this.todoId,
-    status: 'Todo',
-    createdAt: new Date(),
-    description: ""
-  },
-  
-  ]
-  constructor() { }
-  getTodos(){
-    return this.todoList;
+    {
+      title: "Install Angular CLI",
+      id: this.todoId,
+      status: "Todo",
+      createdAt: new Date(),
+      description: ""
+    }
+  ];
+  statuses: string[] = ["Todo", "Doing", "Done"];
+
+  constructor() {}
+  getTodos(status: string) {
+    if(!status){
+      return this.todoList
+    }
+    return this.todoList.filter(todo => todo.status === status);
   }
   deleteTodo(todo: ITodo) {
     const index = this.todoList.findIndex(todoItem => todoItem === todo);
     this.todoList.splice(index, 1);
   }
-  addTodo(todo: ITodo):void {
-    todo.id = this.todoId ++;
+  addTodo(todo: ITodo): void {
+    todo.id = this.todoId++;
     this.todoList.push(todo);
+  }
+  getStatuses() {
+    return this.statuses;
   }
 }
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import {ITodo} from '../interfaces/itodo';
-import {TodoService} from '../services/todo.service';
+import { Component, OnInit, Input } from "@angular/core";
+import { ITodo } from "../interfaces/itodo";
+import { TodoService } from "../services/todo.service";
 
 @Component({
-  selector: 'app-todo-list',
-  templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  selector: "app-todo-list",
+  templateUrl: "./todo-list.component.html",
+  styleUrls: ["./todo-list.component.css"]
 })
 export class TodoListComponent implements OnInit {
-  todoList: ITodo [] = [ ]
-  constructor(private todoService : TodoService) { }
+  @Input() status;
+  constructor(private todoService: TodoService) {}
 
-  ngOnInit() {
-    this.todoList = this.todoService.getTodos();
+  ngOnInit() {}
+
+  get todoList() {
+    return this.todoService.getTodos(this.status);
   }
-
 }
