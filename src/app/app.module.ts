07f583c15c4703e416ff89c96758db22b7a1e160
tcampbe <tcampb@icloud.com>
2020-01-26 21:13:46 -0600

    commented out material imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { TodoListComponent } from "./todo-list/todo-list.component";
 import { CreateTodoComponent } from "./create-todo/create-todo.component";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { ConfirmationModalComponent } from "./confirmation-modal/confirmation-modal.component";
-import { RouterModule } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { BoardComponent } from "./board/board.component";
 import { TodoEditComponent } from "./todo-edit/todo-edit.component";
-
+import { TodoTableComponent } from './todo-table/todo-table.component';
+/* import{MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material';
+ */
 const routes = [];
 const appRoutes = RouterModule.forRoot(routes);
 
@@ -22,14 +25,18 @@ const appRoutes = RouterModule.forRoot(routes);
     CreateTodoComponent,
     ConfirmationModalComponent,
     BoardComponent,
-    TodoEditComponent
+    TodoEditComponent,
+    TodoTableComponent
   ],
 
   imports: [
     BrowserModule, 
     appRoutes, 
     FormsModule, 
-    NgbModule
+    NgbModule,
+/*     MatTableModule,
+    MatSortModule
+ */  
   ],
 
   providers: [],
